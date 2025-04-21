47612d8a207726cb340df9b84e3e8d029eaeb1ed
Harith <120466779+harith3907@users.noreply.github.com>
2025-04-21 16:02:42 +0800

    [04-21-2025] 0657-robot-return-to-origin (Easy)

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves[i] == 'L') {
+                x--;
+            }
+            else if (moves[i] == 'R') {
+                x++;
+            }
+            else if (moves[i] == 'D') {
+                y--;
+            }
+            else if (moves[i] == 'U') {
+                y++;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+};
