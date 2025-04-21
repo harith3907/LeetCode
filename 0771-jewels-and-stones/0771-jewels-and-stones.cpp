712ac73b704910d8932faabcb08b0a9da3a6082e
Harith <120466779+harith3907@users.noreply.github.com>
2025-04-21 23:32:42 +0800

    [04-21-2025] 0771-jewels-and-stones (Easy)

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int jewelCount = 0;
+       for (int i = 0; i < stones.length(); i++) {
+            for (int j = 0; j < jewels.length(); j++) {
+                if (stones[i] == jewels[j]) {
+                    jewelCount++;
+                }
+            }
+       } 
+        return jewelCount;
+    }
+};
